3dc6b76e0698475087d457474987820d5b1bd7f9
Satoshi Kawato <58936715+satoshikawato@users.noreply.github.com>
2025-09-04 17:12:13 +0900

    Update app.py
    rollback

--- app.py
+++ app.py
@@ -37,8 +37,7 @@ FEATURE_KEYS = [
     "telomere", "tmRNA", "transit_peptide", "tRNA", "unsure", "V_region",
     "V_segment", "variation", "3'UTR", "5'UTR"
 ]
-QUALIFIER_KEYS = ["allele", "anticodon", "artificial_location", "bound_moiety", "codon_start", "direction", "EC_number", "estimated_length", "exception", "experiment", "frequency", "function", "gap_type", "gene", "gene_synonym", "GO_function", "GO_process",  "inference", "linkage_evidence", "locus_tag", "mobile_element_type", "mod_base", "ncRNA_class", "note", "number","old_locus_tag",  "operon", "PCR_conditions", "product", "protein_id", "pseudo", "pseudogene", "regulatory_class", "replace", "ribosomal_slippage", "rpt_family", "rpt_type", "rpt_unit_seq", "satellite", "tag_peptide", "translation", "transl_except", "transl_table", "trans_splicing"]
-
+QUALIFIER_KEYS = ["product", "gene", "note", "rpt_family"]
 
 # --- Helper functions and Session State for Dynamic Priority Input ---
 
@@ -1011,5 +1010,6 @@ st.markdown(
     "Source: [gbdraw](https://github.com/satoshikawato/gbdraw)",
     unsafe_allow_html=True
 )
+
 
 
