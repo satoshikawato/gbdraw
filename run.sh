23482e3acedf67320e21338601c1f68435457ba5
satoshikawato <genome.kawato@gmail.com>
2025-12-04 23:45:36 +0900

    updates

--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
+set -e
 
-# --server.enableCORS=false と --server.enableXsrfProtection=false を追加
+nginx
 streamlit run app.py \
-    --server.port=8501 \
-    --server.address=127.0.0.1 \
-    --server.headless=true \
-    --server.enableCORS=false \
-    --server.enableXsrfProtection=false \
-    --server.fileWatcherType=none \
-    --browser.gatherUsageStats=false &
-
-sleep 2
-
-nginx -g 'daemon off;'
-
+  --server.port=8501 \
+  --server.address=0.0.0.0 \
+  --server.enableWebsocketCompression=false
+  
+  
