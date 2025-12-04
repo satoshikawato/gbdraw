10fd3a33f349f77d4bef8d6c33ab4ec6041e0db3
satoshikawato <genome.kawato@gmail.com>
2025-12-04 20:07:43 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,9 @@ nginx -c /app/nginx.conf &
 
 # Streamlitを起動（ポートは8501に固定）
 # maxUploadSizeはここで指定
-streamlit run app.py --server.port=8501 --server.address=127.0.0.1 --server.maxUploadSize=500
+streamlit run app.py \
+    --server.port=8501 \
+    --server.address=127.0.0.1 \
+    --server.maxUploadSize=500 \
+    --server.enableCORS=false \
+    --server.enableXsrfProtection=false
