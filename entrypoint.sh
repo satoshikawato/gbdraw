7907afb332c20cff3f5701c665f7b048a539cf08
satoshikawato <genome.kawato@gmail.com>
2025-12-12 16:08:12 +0900

    test

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Streamlit をバックグラウンド(ポート8501)で起動
-streamlit run app.py --server.port=8501 --server.address=0.0.0.0 --server.headless=true &
-
-# Nginx をフォアグラウンド(ポート8080)で起動
-nginx -g 'daemon off;'
-
