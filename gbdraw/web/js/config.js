5752ac91ff861706e3b97d40fb32cce3d9f34b36
satoshikawato <genome.kawato@gmail.com>
2026-05-07 17:59:46 +0900

    v0.11.0

--- gbdraw/web/js/config.js
+++ gbdraw/web/js/config.js
@@ -1,4 +1,4 @@
-export const GBDRAW_WHEEL_NAME = "gbdraw-0.11.0b0-py3-none-any.whl";
+export const GBDRAW_WHEEL_NAME = "gbdraw-0.11.0-py3-none-any.whl";
 export const GBDRAW_WHEEL_CACHE_BUST = "20260505-175405";
 export const PYODIDE_INDEX_URL = "./vendor/pyodide/v0.29.0/full/";
 export const PYODIDE_LOCAL_WHEELS = [
